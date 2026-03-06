b3c56bf4e730c9211984feff9c26591c1cec7b77
Vissg <vissg4444@gmail.com>
2026-03-06 16:02:58 +0000

    logistic

--- students/kuzmin/lesson2.py
+++ students/kuzmin/lesson2.py
@@ -19,7 +19,10 @@ class LinearRegression:
         return 1 - self.loss(x, y) / np.var(y)  # / дисперсию
 
     def grad(self, x, y) -> tuple[np.ndarray, np.ndarray]:
-        return (-2 / x.shape[0]) * np.dot(x.T, (y - self.predict(x))), -2 * np.mean(y - self.predict(x))
+        p = self.predict(x)
+        dw = (-2 / x.shape[0]) * np.dot(x.T, (y - p))
+        db = -2 * np.mean(y - p)
+        return dw, db
 
 
 class LogisticRegression:
@@ -31,16 +34,22 @@ class LogisticRegression:
         self.bias = np.array(0.0)
 
     def predict(self, x: np.ndarray) -> np.ndarray:
-        return np.dot(x, self.weights) + self.bias
+        z = np.dot(x, self.weights) + self.bias
+        return 1 / np.exp(-z)
 
     def loss(self, x: np.ndarray, y: np.ndarray) -> float:
-        return np.sum(-(y * np.log(self.predict(x)) + (1 - y) * np.log(1 - self.predict(x))))
+        return -np.sum(y * np.log(self.predict(x)) + (1 - y) * np.log(1 - self.predict(x)))
 
     def metric(self, x: np.ndarray, y: np.ndarray) -> float:
-        return 0  # точности
+        return np.mean((self.predict(x) >= 0.5) == y)
 
     def grad(self, x, y) -> tuple[np.ndarray, np.ndarray]:
-        return np.zeros_like(self.weights), np.zeros_like(self.bias) # посчитать
+
+        p = self.predict(x)
+        f = p / (p + 1)
+        db = f * (1 - f)
+        dw = x * db
+        return dw, db
 
 
 class Exercise:
@@ -61,10 +70,8 @@ class Exercise:
         return LogisticRegression(num_features, rng or np.random.default_rng())
 
     @staticmethod
-    def fit(
-        model: LinearRegression | LogisticRegression, x: np.ndarray, y: np.ndarray, lr: float, n_iter: int
-    ) -> None:
+    def fit(model: LinearRegression | LogisticRegression, x: np.ndarray, y: np.ndarray, lr: float, n_iter: int) -> None:
         for _ in range(n_iter):
-            model.weights -= lr * model.grad(x, y)[0]
-            model.bias -= lr * model.grad(x, y)[1]
-
+            dw, db = model.grad(x, y)
+            model.weights -= lr * dw
+            model.bias -= lr * db
