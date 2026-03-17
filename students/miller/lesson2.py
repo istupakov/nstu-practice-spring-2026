4b5e11baedcf44920a10a6305a31a9a12b62d6e6
Miller Igor <i.miller.nstu@gmail.com>
2026-03-17 21:35:36 +0300

    Lesson 2.2 1&2

--- students/miller/lesson2.py
+++ students/miller/lesson2.py
@@ -17,9 +17,33 @@ class LinearRegression:
         mse = np.mean((pred - y) ** 2)
         return float(mse)
 
-    def metric(self, x: np.ndarray, y: np.ndarray) -> float:
+    def metric(self, x: np.ndarray, y: np.ndarray, type: str = "precision") -> float:
         pred = self.predict(x)
-        return float(1 - (np.sum((y - pred) ** 2) / np.sum((y - np.mean(y)) ** 2)))
+        tp = np.sum ((pred == 1) & (y == 1))
+        fp = np.sum ((pred == 1) & (y == 0))
+        fn = np.sum ((pred == 0) & (y == 1))
+        tn = np.sum ((pred == 0) & (y == 0))
+        if type == "precision":
+            return tp/(tp+fp) 
+        if type == "recall":
+            return tp/(tp+fn)
+        if type == "f1":
+            return tp/(tp + 1/2*(fp+fn))
+        if type == "auroc":
+            ind = np.argsort(pred)[::-1]
+            y_sorted = y[ind]
+            total_tp = np.sum(y == 1)
+            total_tn = np.sum(y == 0)
+            tpr = []
+            fpr = []
+            for i in range(len(pred)):
+                tp_i = np.sum(y_sorted[:i+1] == 1)
+                fp_i = np.sum(y_sorted[:i+1] == 0)
+            
+                tpr.append(tp_i / total_tp)
+                fpr.append(fp_i / total_tn)
+            
+            return np.trapezoid(tpr,fpr)
 
     def grad(self, x, y) -> tuple[np.ndarray, np.ndarray]:
         pred = self.predict(x)
@@ -75,8 +99,24 @@ class Exercise:
         return LogisticRegression(num_features, rng or np.random.default_rng())
 
     @staticmethod
-    def fit(model: LinearRegression | LogisticRegression, x: np.ndarray, y: np.ndarray, lr: float, n_iter: int) -> None:
+    def fit(
+        model: LinearRegression | LogisticRegression,
+        x: np.ndarray,
+        y: np.ndarray,
+        lr: float,
+        n_iter: int,
+        batch_size: int | None,
+    ) -> None:
         for _iteration in range(n_iter):
-            dw, db = model.grad(x, y)
-            model.weights -= lr * dw
-            model.bias -= lr * db
+            if batch_size is None:
+                dw, db = model.grad(x, y)
+                model.weights -= lr * dw
+                model.bias -= lr * db
+            else:
+                n = x.shape[0]
+                ind = np.random.choice(n, batch_size, replace=False)
+                x_batch = x[ind]
+                y_batch = y[ind]
+                dw, db = model.grad(x_batch, y_batch)
+                model.weights -= lr * dw
+                model.bias -= lr * db
