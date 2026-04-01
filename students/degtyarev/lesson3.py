5c21da03ddd0da2c221af7511f519c045c5f07cc
Kirill <kirlint@icloud.com>
2026-04-01 11:28:17 +0000

    Lesson 3 upd.

--- students/degtyarev/lesson3.py
+++ students/degtyarev/lesson3.py
@@ -16,17 +16,23 @@ class Layer(Protocol):
     def grad(self) -> Sequence[np.ndarray]: ...
 
 
+class Loss(Protocol):
+    def forward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray: ...
+
+    def backward(self) -> np.ndarray: ...
+
+
 class LinearLayer(Layer):
     def __init__(self, in_features: int, out_features: int, rng: np.random.Generator | None = None) -> None:
         if rng is None:
             rng = np.random.default_rng()
+
         k = np.sqrt(1 / in_features)
         self.weights = rng.uniform(-k, k, (out_features, in_features)).astype(np.float32)
         self.bias = rng.uniform(-k, k, out_features).astype(np.float32)
 
-        self.d_weights = np.zeros_like(self.weights)
-        self.d_bias = np.zeros_like(self.bias)
-        # Инициализируем пустым массивом вместо None, чтобы тайп-чекер не ругался
+        self._d_weights = np.zeros_like(self.weights)
+        self._d_bias = np.zeros_like(self.bias)
         self._input_cache = np.array([])
 
     def forward(self, x: np.ndarray) -> np.ndarray:
@@ -34,8 +40,8 @@ class LinearLayer(Layer):
         return x @ self.weights.T + self.bias
 
     def backward(self, dy: np.ndarray) -> np.ndarray:
-        self.d_weights = dy.T @ self._input_cache
-        self.d_bias = np.sum(dy, axis=0)
+        self._d_weights = dy.T @ self._input_cache
+        self._d_bias = np.sum(dy, axis=0)
         dx = dy @ self.weights
         return dx
 
@@ -45,7 +51,7 @@ class LinearLayer(Layer):
 
     @property
     def grad(self) -> Sequence[np.ndarray]:
-        return (self.d_weights, self.d_bias)
+        return (self._d_weights, self._d_bias)
 
 
 class ReLULayer(Layer):
@@ -145,10 +151,78 @@ class Model(Layer):
         return tuple(grads)
 
 
+class MSELoss(Loss):
+    def __init__(self) -> None:
+        self._x = np.array([])
+        self._y = np.array([])
+
+    def forward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        self._x = x
+        self._y = y
+        return np.array(np.mean((x - y) ** 2), dtype=np.float32)
+
+    def backward(self) -> np.ndarray:
+        return 2.0 * (self._x - self._y) / self._x.size
+
+
+class BCELoss(Loss):
+    def __init__(self) -> None:
+        self._x = np.array([])
+        self._y = np.array([])
+
+    def forward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        self._x = x
+        self._y = y
+        loss = -np.mean(y * np.log(x) + (1 - y) * np.log(1 - x))
+        return np.array(loss)
+
+    def backward(self) -> np.ndarray:
+        batch_size = self._x.shape[0]
+        return (self._x - self._y) / (self._x * (1 - self._x)) / batch_size
+
+
+
+class NLLLoss(Loss):
+    def __init__(self) -> None:
+        self._dx = np.array([])
+
+    def forward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        batch_size = x.shape[0]
+        hot_y = np.zeros_like(x)
+        hot_y[np.arange(batch_size), y] = 1
+        self._dx = -hot_y / batch_size
+        loss = -np.sum(x * hot_y) / batch_size
+        return np.array(loss, dtype=np.float32)
+
+    def backward(self) -> np.ndarray:
+        return self._dx
+
+
+class CrossEntropyLoss(Loss):
+    def __init__(self) -> None:
+        self._dx = np.array([])
+        self._loss = np.array(0.0)
+
+    def forward(self, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        batch_size = x.shape[0]
+
+        shifted = x - np.max(x, axis=-1, keepdims=True)
+        logprobs = shifted - np.log(np.sum(np.exp(shifted), axis=-1, keepdims=True))
+
+        hot_y = np.zeros_like(x)
+        hot_y[np.arange(batch_size), y] = 1
+
+        self._loss = -np.sum(logprobs * hot_y) / batch_size
+        self._dx = (np.exp(logprobs) - hot_y) / batch_size
+        return np.array(self._loss)
+
+    def backward(self) -> np.ndarray:
+        return self._dx
+
+
 class Exercise:
     @staticmethod
     def get_student() -> str:
-        # ЗАМЕНИ НА СВОЕ ИМЯ
         return "Дегтярев Кирилл Романович, ПМ-35"
 
     @staticmethod
@@ -174,3 +248,33 @@ class Exercise:
     @staticmethod
     def create_model(*layers: Layer) -> Layer:
         return Model(*layers)
+
+    @staticmethod
+    def create_mse_loss() -> Loss:
+        return MSELoss()
+
+    @staticmethod
+    def create_bce_loss() -> Loss:
+        return BCELoss()
+
+    @staticmethod
+    def create_nll_loss() -> Loss:
+        return NLLLoss()
+
+    @staticmethod
+    def create_cross_entropy_loss() -> Loss:
+        return CrossEntropyLoss()
+
+    @staticmethod
+    def train_model(
+        model: Layer, loss: Loss, x: np.ndarray, y: np.ndarray, lr: float, n_epoch: int, batch_size: int
+    ) -> None:
+        idx = np.arange(batch_size, x.shape[0], batch_size)
+
+        for _ in range(n_epoch):
+            for x_batch, y_batch in zip(np.split(x, idx, axis=0), np.split(y, idx, axis=0), strict=True):
+                loss.forward(model.forward(x_batch), y_batch)
+                model.backward(loss.backward())
+
+                for p, g in zip(model.parameters, model.grad, strict=True):
+                    p += -lr * g
